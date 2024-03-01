c19dd0f4b1e23b94446e3d42a00a1345fa5573bd
Jowkem <pimkote1615@gmail.com>
2024-03-02 00:43:08 +0700

    Add Authentication

--- controllers/auth.js
+++ controllers/auth.js
@@ -11,7 +11,7 @@ exports.register = async (req,res,next) => {
         const user = await User.create({
             name, email, password, role
         });
-        //const token = user.getSingedJwtToken();
+        //const token = user.getSignedJwtToken();
         //res.status(200).json({success:true, token});
         sendTokenResponse(user,200,res);
 
@@ -46,7 +46,7 @@ exports.login = async (req,res,next) => {
     }
 
     //Create token
-    //const token = user.getSingedJwtToken();
+    //const token = user.getSignedJwtToken();
     //res.status(200).json({success:true, token});
     sendTokenResponse(user,200,res);
     } catch(err) {
@@ -57,7 +57,7 @@ exports.login = async (req,res,next) => {
 //Get token from model, create cookie and send response
 const sendTokenResponse = (user, statusCode, res) => {
     //Create token
-    const token = user.getSingedJwtToken();
+    const token = user.getSignedJwtToken();
 
     const options = {
         expires: new Date (Date.now()+process.env.JWT_COOKIE_EXPIRE*24*60*60*1000),
